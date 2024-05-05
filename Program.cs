36f267c2ad47b89d7c174e2d2262f85dc11f1553
Yaroslav Parkhomenko <YaroslavParkhomenkoPetrovich@gmail.com>
2024-05-06 02:53:56 +0300

    Changed modificator of Main() method. Added console user in-put for variable age to use it for Age property in the User class instance (instantinates User).

--- Program.cs
+++ Program.cs
@@ -2,9 +2,14 @@ namespace DZ_03
 {
     internal class Program
     {
-        public static void Main(string[] agrs)
+        static void Main(string[] agrs)
         {
-            
+            // CONSOLE INPUT OF AGE.
+            Console.Write("PEASE, ENTER THE AGE: ");
+            byte age = byte.Parse(Console.ReadLine());
+
+            User user = new User();     // THE CREATION OF User-CLASS INSTANCE WITH DEFAULT CONSTRUCTOR.
+            User user1 = new User(age);     // CREATION OF User-CLASS INSTANCE WITH CONSTRUCTOR WHICH HAS PARAMETER Age = age WITH init IN PROPERTY, INITIALIZATION ONLY ONCE AND THEN Age IS IMMUTABLE.
         }
     }
 }
