adda9ede994030a10b91f6d6fa2d7c0a9ea57fb5
Yaroslav Parkhomenko <YaroslavParkhomenkoPetrovich@gmail.com>
2024-05-06 07:21:54 +0300

    The default initializations deleted from auto-implemented properties and they moved into constructors, because of the attmpt to make less work for GarbageCollector program. If the properties may be Value Types, I think the default value can be left in auto-implemented properties thmeselves.

--- User.cs
+++ User.cs
@@ -6,8 +6,8 @@ namespace DZ_03
     {
         // PROPERTIES
 
-        public string FirstName { get; set; } = "NO_NAME";      // INITIAL VALUE IS "NO_NAME".
-        public string LastName { get; set; } = "NO_NAME";
+        public string FirstName { get; set; }     // INITIAL VALUE IS "NO_NAME".
+        public string LastName { get; set; }
         // USING OF KEYWORD init TO INITIALIZE Age PROPERTY ONLY ONCE, AND THEN MAKE IT immutable.
         public byte Age { get; init; }
         // THE CREATION OF Gender PROPERTY WHICH CORRESPONDS TO enum UserGender TYPE.
@@ -27,12 +27,20 @@ namespace DZ_03
         public User()
         {
             Console.WriteLine("THE DEFAULT CONSTRUCTOR OF User INITIALIZED!");
+
+            // DEFAULT VALUES OF PROPERTIES.
+            this.FirstName = "NO_NAME";
+            this.LastName = "NO_NAME";
         }
 
         // CONSTRUCTOR WICH SETUPS Age PROPERTY.
         [SetsRequiredMembers]    // PARAMETER THAT USED CONFIGURE CONSTRUCTOR TO SHOW THAT SETUP AGE WITH DEFAULT VALUE WILL BE DONE IN ANY CASE, AND THAT BECAUSE CALLER IS NOT FORCED TO ASSIGN VALUE ON COMPILATION STAGE... :S AT LEAST AS I HAVE UNDERSTAND.
         public User(byte age)
         {
+            // DEFAULT VALUES OF PROPERTIES.
+            this.FirstName = "NO_NAME";
+            this.LastName = "NO_NAME";
+
             this.Age = age;     // ASSIGN PROPERTY Age VIA CONSTRUCTOR METHOD.
         }
     }
