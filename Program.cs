069fa80310ada52d51ebf484de198f690f4844c1
Yaroslav Parkhomenko <YaroslavParkhomenkoPetrovich@gmail.com>
2024-05-06 08:08:41 +0300

    To add try-catch finally construction to handle exception of the user console input.

--- Program.cs
+++ Program.cs
@@ -1,3 +1,5 @@
+using System;
+
 namespace DZ_03
 {
     internal class Program
@@ -7,27 +9,43 @@ namespace DZ_03
             User user = new User();     // THE CREATION OF User-CLASS INSTANCE WITH DEFAULT CONSTRUCTOR.
             Console.WriteLine();
 
-            // CONSOLE INPUT OF AGE.
-            Console.Write("PEASE, ENTER THE AGE: ");
-            User user1 = new User(byte.Parse(Console.ReadLine()));     // CREATION OF User-CLASS INSTANCE WITH CONSTRUCTOR WHICH HAS PARAMETER Age = age WITH init IN PROPERTY, INITIALIZATION ONLY ONCE AND THEN Age IS IMMUTABLE.
-            Console.WriteLine();
+            // THE try-catch-finally CONSTRUCTION TO HANDLE EXCEPTIONS.
+            try
+            {
+                // CONSOLE INPUT OF AGE.
+                Console.Write("PEASE, ENTER THE AGE: ");
+                User user1 = new User(byte.Parse(Console.ReadLine()));     // CREATION OF User-CLASS INSTANCE WITH CONSTRUCTOR WHICH HAS PARAMETER Age = age WITH init IN PROPERTY, INITIALIZATION ONLY ONCE AND THEN Age IS IMMUTABLE.
+                Console.WriteLine();
 
-            // CONSOLE INPUT OF GENDER AND ASSIGNMENT IT TO THE INSTANCE OF THE CLASS.
-            Console.Write("PLEASE, ENTER THE GENDER (0 — UNKNOWN, 10 — MALE, 11 — FEMALE): ");
-            user1.Gender = (UserGender)byte.Parse(Console.ReadLine());
-            Console.WriteLine();
+                // CONSOLE INPUT OF GENDER AND ASSIGNMENT IT TO THE INSTANCE OF THE CLASS.
+                Console.Write("PLEASE, ENTER THE GENDER (0 — UNKNOWN, 10 — MALE, 11 — FEMALE): ");
+                user1.Gender = (UserGender)byte.Parse(Console.ReadLine());
+                Console.WriteLine();
 
-            // THE CONSOLE INPUT AND ASSIGNMENT INPUT OF THE STRING PROPERTIES FirstName AND LastName.
-            Console.Write("PLEASE, ENTER THE FIRST NAME: ");
-            user1.FirstName = Console.ReadLine();
-            Console.WriteLine();
+                // THE CONSOLE INPUT AND ASSIGNMENT INPUT OF THE STRING PROPERTIES FirstName AND LastName.
+                Console.Write("PLEASE, ENTER THE FIRST NAME: ");
+                user1.FirstName = Console.ReadLine();
+                Console.WriteLine();
 
-            Console.Write("PLEASE, ENTER THE LAST NAME: ");
-            user1.LastName = Console.ReadLine();
-            Console.WriteLine();
+                Console.Write("PLEASE, ENTER THE LAST NAME: ");
+                user1.LastName = Console.ReadLine();
+                Console.WriteLine();
 
-            // SHOW INFORMATION ABOUT THE INSTANCE VIA CONSOLE.
-            Console.WriteLine(user1.ToString());
+                // TO SHOW INFORMATION ABOUT THE INSTANCE VIA CONSOLE.
+                Console.WriteLine(user1.ToString());
+            }
+            catch (FormatException formatException)
+            {
+                Console.WriteLine($"{formatException.Message}");
+            }
+            catch (OverflowException rangeException)
+            {
+                Console.WriteLine($"{rangeException.Message}");
+            }
+            catch
+            {
+                Console.WriteLine("A CRITICAL ERROR APPEARED!!!");
+            }
         }
     }
 }
