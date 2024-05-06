6cfbf5a56effa83c55c05806312b53a60e091751
Yaroslav Parkhomenko <YaroslavParkhomenkoPetrovich@gmail.com>
2024-05-06 07:30:08 +0300

    The check to make a different output for the underages added into ToString() method. The small comment and the small fix into the output are added.

--- User.cs
+++ User.cs
@@ -11,14 +11,22 @@ namespace DZ_03
         // USING OF KEYWORD init TO INITIALIZE Age PROPERTY ONLY ONCE, AND THEN MAKE IT immutable.
         public byte Age { get; init; }
         // THE CREATION OF Gender PROPERTY WHICH CORRESPONDS TO enum UserGender TYPE.
-        public UserGender Gender { get; set; }
+        public UserGender Gender { get; set; }      // THE DEFAULT VALUE IS Unknown = 0.
 
         // METHODS
 
         // THE METHOD ToString() FROM System.Object OVERRIDED TO MAKE PROPER OUTPUT FOR INSTANCES OF User.
         public override string ToString()
         {
-            return $"Hi, my name is {FirstName} and last name {LastName}. I am {20} years old. I am {Gender}.";
+            // THE CHECK TO MAKE A PROPER OUT PUT FOR THE UNDERAGES.
+            if (this.Age <= 10)
+            {
+                return $"Hi, my name is {this.FirstName} and last name {this.LastName}. I am {this.Age} years old. I am {this.Gender}.";
+            }
+            else
+            {
+                return $"Hi, my name is {this.FirstName} and last name {this.LastName}. I am a baby. I am {this.Gender}.";
+            }
         }
 
         // CONSTRUCTORS
