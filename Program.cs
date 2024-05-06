0c08911ff23e702a2ba71fcc17a767862a138da4
Yaroslav Parkhomenko <YaroslavParkhomenkoPetrovich@gmail.com>
2024-05-06 07:34:48 +0300

    The console output of general information about the class instance added to Program.cs file.

--- Program.cs
+++ Program.cs
@@ -24,6 +24,9 @@ namespace DZ_03
 
             Console.Write("PLEASE, ENTER THE LAST NAME: ");
             user1.LastName = Console.ReadLine();
+
+            // SHOW INFORMATION ABOUT THE INSTANCE VIA CONSOLE.
+            Console.WriteLine(user1.ToString());
         }
     }
 }
