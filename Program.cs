1b5f02088563423bd003b9b59bed4cfb8d521bf1
Yaroslav Parkhomenko <YaroslavParkhomenkoPetrovich@gmail.com>
2024-05-22 12:18:32 +0300

    Add loop console input with finite attempts. Add variables correspondly. Edit validation method correspondly.

--- Program.cs
+++ Program.cs
@@ -51,63 +51,168 @@ namespace DZ_03
 
             // THE try-catch-finally CONSTRUCTION TO HANDLE EXCEPTIONS.
             try
-            {   
+            {
+                // VARIABLES FOR LOOP OF CONSOLE INPUT.
+                int inputCount = 5;
+                bool isValidAge = false;
+                bool isValidFirstName = false;
+                bool isValidLastName = false;
+                bool isValidGender = false;
+                string firstNameLoop = string.Empty;
+                string lastNameLoop = string.Empty;
+                UserGender genderLoop = UserGender.unknown;
+                bool isValidationSuccess = false; // TO MAKE LOGIC MORE READABLE.
+
+                // VARIABLE WHICH ARE ARGUMENTS FOR A CONSTRUCTOR.
+                byte age = 0;
+                UserGender gender = UserGender.unknown;
+                string firstName = string.Empty;
+                string lastName = string.Empty;
+
                 // INPUT AND VALIDATION OF firstName AND lastName
-                
-                Console.Write("PLEASE, ENTER THE FIRST NAME: ");
-                string firstName = Console.ReadLine();
-                Console.WriteLine();
 
-                ValidateName(firstName);
-                firstName = DeleteSpacesAfterBefore(firstName);
-
-                Console.Write("PLEASE, ENTER THE LAST NAME: ");
-                string lastName = Console.ReadLine();
-                Console.WriteLine();
-
-                ValidateName(lastName);
-                lastName = DeleteSpacesAfterBefore(lastName);
-
-                // CONSOLE INPUT OF AGE.
-                Console.Write("PLEASE, ENTER THE AGE: ");
-                byte age = byte.Parse(Console.ReadLine());
-                Console.WriteLine();
-
-                // IF AGE IS TOO OLD, OR TOO YOUNG THEN TERMINATE THE PROGRAM.
-                if ((age >= 110) || (age <= 3))
+                do
                 {
-                    throw new OverflowException();
+                    if (isValidFirstName == false)
+                    {
+                        Console.Write($"PLEASE, ENTER THE FIRST NAME ({inputCount} — ATTEMPTS LEFT): ");
+                        firstNameLoop = Console.ReadLine();
+                        Console.WriteLine();
+
+                        isValidFirstName = ValidateName(firstNameLoop);
+                        
+                        // IF INPUT IS NOT VALID TO START LOOP OVER.
+                        if (isValidFirstName == false)
+                        {
+                            inputCount = inputCount - 1;
+                            continue;
+                        }
+                    }
+                    
+                    // TO NOT ASSIGN EVERY TIME WE SHOULD CHECK IF TWO VALUES EQUALS.
+                    if ((isValidFirstName == true) && (firstName != firstNameLoop))
+                    {
+                        firstName = firstNameLoop;
+                    }
+
+                    if (isValidLastName == false)
+                    {
+                        Console.Write($"PLEASE, ENTER THE LAST NAME ({inputCount} — ATTEMPTS LEFT): ");
+                        lastNameLoop = Console.ReadLine();
+                        Console.WriteLine();
+
+                        isValidLastName = ValidateName(lastNameLoop);
+
+                        if (isValidLastName == false)
+                        {
+                            inputCount = inputCount - 1;
+                            continue;
+                        }
+                    }
+
+                    if ((isValidLastName == true) && (lastName != lastNameLoop))
+                    {
+                        lastName = lastNameLoop;
+                    }
+
+                    // VALIDATION OF AGE.
+                    if (isValidAge == false)
+                    {
+                        // CONSOLE INPUT OF AGE.
+                        Console.Write($"PLEASE, ENTER THE AGE ({inputCount} — ATTEMPTS LEFT): ");
+                        isValidAge = byte.TryParse(Console.ReadLine(), out age);
+                        Console.WriteLine();
+
+                        // IF AGE IS IN THE BORDERS.
+                        if ((isValidAge) && ((age <= 110) && (age >= 3)))
+                        {
+                            isValidAge = true;
+                        }
+                        else
+                        {
+                            isValidAge = false;
+                            inputCount = inputCount - 1;
+                            continue;
+                        }
+                    }
+
+                    // CONSOLE INPUT AND PARSING OF GENDER THAN CONVER IT INTO CORRESPONDING enum. ToLower() USED BECAUSE IN UserGender STRING VALUE ARE IN THE LOWER CASE.
+                    if (isValidGender == false)
+                    {
+                        Console.Write($"PLEASE, ENTER THE GENDER (0 — UNKNOWN, 10 — MALE, 11 — FEMALE) ({inputCount} — ATTEMPTS LEFT): ");
+                        try
+                        {
+                            genderLoop = (UserGender)Enum.Parse(typeof(UserGender), Console.ReadLine().ToLower());
+                        }
+                        catch (ArgumentException argumentException)
+                        {
+                            Console.WriteLine(argumentException.Message);
+                            inputCount = inputCount - 1;
+                            continue;
+                        }
+                        catch
+                        {
+                            Console.WriteLine("CHOOSE THE CORRECT VARIANT OF GENDER!!!");
+                            inputCount = inputCount - 1;
+                            continue;
+                        }
+
+                        Console.WriteLine();
+
+                        #region
+                        // IF GENDER IS SOMETHING DIFFERENT FROM GENDER ENUMERATION, TERMINATE THE PROGRAM.
+                        // IF IsDefined() METHOD RETURN TRUE — THE PROPER GENDER SELECTED FROM UserGender ENUMERATION, AND DUE TO THE NEGATION OPERATOR THE PROGRAM AVOIDS THE CODE INSIDE if() CONSTRUCTION. ELSE TERMINATE THE PROGRAM.
+                        // https://learn.microsoft.com/en-us/dotnet/api/system.enum.isdefined?view=net-8.0
+                        #endregion
+                        isValidGender = Enum.IsDefined(typeof(UserGender), genderLoop);
+
+                        if (isValidGender == false)
+                        {
+                            inputCount = inputCount - 1;
+                            continue;
+                        }
+                    }
+
+                    if ((isValidGender == true) && (gender != genderLoop))
+                    {
+                        gender = genderLoop;
+                    }
+
+                    // EVALUEATE INDICATOR VARIABLE.
+                    isValidationSuccess = (isValidAge && isValidFirstName && isValidLastName && isValidGender);
+
+                    // DECREMENT OF THE LOOP INDEX.
+                    inputCount = inputCount - 1;
+                }
+                while ((inputCount > 0) || (isValidationSuccess == false));
+
+                // IF VALIDATION OF THE USER INPUT WAS SUCCESSFUL, INVOKE THE CONSTRUCTOR.
+                if (isValidationSuccess)
+                {
+                    // WHITESPACES ERASING.
+                    firstName = DeleteSpacesAfterBefore(firstName);
+                    lastName = DeleteSpacesAfterBefore(lastName);
+
+                    User user1 = new User(age)
+                    {
+                        FirstName = firstName,
+                        LastName = lastName,
+                        Gender = gender
+                    };
+
+                    Console.WriteLine("THE PARAMETRIC CONSTRUCTOR OF User INITIALIZED!");
+
+                    Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+
+                    // TO SHOW INFORMATION ABOUT THE INSTANCE VIA CONSOLE.
+                    Console.WriteLine(user1.ToString());
+                }
+                else
+                {
+                    Console.WriteLine("AN ERROR OCCURED!!!");
+                    return;
                 }
 
-                // CONSOLE INPUT AND PARSING OF GENDER THAN CONVER IT INTO CORRESPONDING enum. ToLower() USED BECAUSE IN UserGender STRING VALUE ARE IN THE LOWER CASE.
-                Console.Write("PLEASE, ENTER THE GENDER (0 — UNKNOWN, 10 — MALE, 11 — FEMALE): ");
-                UserGender gender = (UserGender)Enum.Parse(typeof(UserGender), Console.ReadLine().ToLower());
-
-                Console.WriteLine();
-
-                #region
-                // IF GENDER IS SOMETHING DIFFERENT FROM GENDER ENUMERATION, TERMINATE THE PROGRAM.
-                // IF IsDefined() METHOD RETURN TRUE — THE PROPER GENDER SELECTED FROM UserGender ENUMERATION, AND DUE TO THE NEGATION OPERATOR THE PROGRAM AVOIDS THE CODE INSIDE if() CONSTRUCTION. ELSE TERMINATE THE PROGRAM.
-                // https://learn.microsoft.com/en-us/dotnet/api/system.enum.isdefined?view=net-8.0
-                #endregion
-                if (!Enum.IsDefined(typeof(UserGender), gender))
-                {
-                    throw new FormatException();
-                }
-
-                User user1 = new User(age)
-                {
-                    FirstName = firstName,
-                    LastName = lastName,
-                    Gender = gender
-                };
-
-                Console.WriteLine("THE PARAMETRIC CONSTRUCTOR OF User INITIALIZED!");
-
-                Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
-
-                // TO SHOW INFORMATION ABOUT THE INSTANCE VIA CONSOLE.
-                Console.WriteLine(user1.ToString());
             }
             catch (FormatException formatException)
             {
@@ -138,28 +243,21 @@ namespace DZ_03
             return name;
         }
 
-        internal static void ValidateName(string name)
+        internal static bool ValidateName(string name)
         {
-            // THE PROGRAM CHECKS INPUT FOR name ON EMPTY LINE, ONLY WHITE SPACES AND NULL.
-            if (String.IsNullOrEmpty(name))
-            {
-                throw new FormatException();
-            }
-
-            // IF name IS TOO LONG, THEN THROW THE EXCEPTION.
-            if (name.Length >= 64)
-            {
-                throw new FormatException();
-            }
-
             // A VALIDATION WITH HELP OF REGULAR EXPRESSIONS.
             // JUST TO MAKE THE SOURCE CODE MORE READABLE, LOCALIZE REGEX IN THE BOOL VALUE.
             // 0 — MEANS NO OPTION IS SET.
             bool nameIsMatch = Regex.IsMatch(name, _patternNameRegex, RegexOptions.IgnoreCase, TimeSpan.FromMilliseconds(_timeoutRegexCheckup));
 
-            if (nameIsMatch)
+            // THE PROGRAM CHECKS INPUT FOR name ON EMPTY LINE, ONLY WHITE SPACES AND NULL. AND IT CHECKS THE LENGTH. AND IT PERFORMS REGULAR EXPRESSION CHECKS.
+            if ((String.IsNullOrEmpty(name)) || (name.Length >= 64) || nameIsMatch)
             {
-                throw new FormatException();
+                return false;
+            }
+            else
+            {
+                return true;
             }
         }
     }
